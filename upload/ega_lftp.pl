2166b72526f97d70ee9171ed090d4a94db5b1a9d
lheisler <lheisler.oicr.on.ca>
2017-07-14 12:05:20 -0400

    corrections to ega_lftp.pl after testing

--- upload/ega_lftp.pl
+++ upload/ega_lftp.pl
@@ -6,6 +6,14 @@ use File::Basename;
 use Data::Dumper;
 
 
+### capture the command line for next option
+my $commandline = join " ", $0, @ARGV;
+my $pwd=`pwd`;chomp $pwd;
+
+
+
+
+
 ### script to encrypt and upload data to ega
 ### requires the following information, which can be stored in a configuration file
 ### folder with files to upload
@@ -14,79 +22,109 @@ use Data::Dumper;
 ### path on the ega box
 ### flag indicating that encrypted data should be deleted after upload
 
-use Config::General qw(ParseConfig);
+#use Config::General qw(ParseConfig);
 use Getopt::Long;
 my %opts = ();
 GetOptions(
-	"config=s" 		=> \$opts{"configfile"},   ### configuration file where options are stored
-	"dir_queue=s" 	=> \$opts{"qdir"},         ### folder with links to files that need encryption/upload
-	"dir_work=s" 	=> \$opts{"wdir"},         ### folder where work should be done, usually in scratch
-	"box=s" 		=> \$opts{"box"},  
-	"box_pw=s" 		=> \$opts{"pass"},
-	"box_path=s"	=> \$opts{"path"},  
-	"delete=s"		=> \$opts{"delete"},
-	"next=s"		=> \$opts{"next"},
+	"config=s" 		=> \$opts{configfile},   ### configuration file where options are stored
+	"qdir=s" 	=> \$opts{qdir},         ### folder with links to files that need encryption/upload
+	"wdir=s" 	=> \$opts{wdir},         ### folder where work should be done, usually in scratch
+	"box=s" 		=> \$opts{box},  
+	"pw=s" 		=> \$opts{pw},
+	"boxpath=s"	=> \$opts{boxpath},  
+	"delete=s"		=> \$opts{delete},
+	"next=s"		=> \$opts{next},
 	"help" 			=> \$opts{help},
 	"keys"			=> \$opts{keys},          ### comma separated list of public encryption keys
 );
-
-### configuration options are overwritten by command line options
-my %config = ParseConfig($opts{"configfile"});
-map{$opts{$_} = $opts{$_} || $config{$_}} keys %config;
-validate_options(\%opts);
+%opts=validate_options(%opts);
 
 
+### grab ONE file from the queue direcotry
+my $file=`ls $opts{qdir} | head -n 1`;chomp $file;   ### grab one file
+my $fn=basename($file);
+print STDERR "Processing next file in queue, $fn\n";
 
+my $workdir=$opts{wdir} . "/$fn";
+usage("$workdir already exists.  Upload of this file has completed.") if(-d $workdir);
+mkdir $workdir;
 
-#my $pwd=`pwd`;chomp $pwd;
-#print STDERR "starting new job\n";
+print STDERR "Moving $file to $workdir for processing\n";
+`mv to_upload/$file $workdir/$fn`;
 
-#my $remote_dir="/haltwg/bam";
+print STDERR "Preparing process script under $workdir\n";
 
-#my $file=`ls to_upload | head -n 1`;chomp $file;   ### grab one file
-#my $fn=basename($file);
-#(my $id=$fn)=~s/\.fastq.*//;
-#my $dir="scratch/$id";
-#die "directory already exists for $id in scratch" if(-d $dir);
-#mkdir $dir;
-#print "$file\n";
-#`mv to_upload/$file $dir/$file`;
+my $script_process="$workdir/process.sh";
+my @keys=split /,/,$opts{keys};
+my $keystring=join(" ",map{"-r $_"} @keys);
  
-#(open my $QSUB,">","$dir/process.sh") || die "could not open upload file";
-#print $QSUB "qsub -cwd -b y -e $pwd/logs -o $pwd/logs -N ega.$id.premd5                              \"md5sum $fn | cut -f1 -d\' \'> $fn.md5\"\n";
-#print $QSUB "qsub -cwd -b y -e $pwd/logs -o $pwd/logs -N ega.$id.gpg  	                             \"gpg --trust-model always -r EGA_Public_key -r SeqProdBio -o $fn.gpg -e $fn\"\n";
-#print $QSUB "qsub -cwd -b y -e $pwd/logs -o $pwd/logs -N ega.$id.postmd5   -hold_jid ega.$id.gpg     \"md5sum $fn.gpg | cut -f1 -d\' \'> $fn.gpg.md5\"\n";
-#print $QSUB "qsub -cwd -b y -e $pwd/logs -o $pwd/logs -N ega.$id.size      -hold_jid ega.$id.gpg     \"du $fn.gpg > $fn.gpg.size \"\n";
-#print $QSUB "qsub -cwd -b y -e $pwd/logs -o $pwd/logs -N ega.$id.upload    -hold_jid ega.$id.postmd5 \"bash upload.sh\"\n";
-#print $QSUB "qsub -cwd -b y -e $pwd/logs -o $pwd/logs -N ega.$id.rmgpg     -hold_jid ega.$id.upload  \"rm $fn.gpg;\"\n";
-#print $QSUB "qsub      -b y -e $pwd/logs -o $pwd/logs -N ega.$id.finish    -hold_jid ega.$id.rmgpg   \"mv $pwd/$dir $pwd/uploaded/$id \"\n";
-#print $QSUB "qsub -wd $pwd -b y -e $pwd/logs -o $pwd/logs -N ega.$id.startnext -hold_jid ega.$id.finish  \"sleep 5m;./ega.pl\"\n"; 
-#print $QSUB "qsub -wd $pwd -b y -e $pwd/logs -o $pwd/logs -N ega.$id.startnext -hold_jid ega.$id.upload  \"sleep 5m;$pwd/ega.lftp.pl\"\n";
-#close $QSUB;
+(open my $QSUB,">",$script_process) || die "could not open $script_process";
+print $QSUB "qsub -cwd -b y -N ega.$fn.premd5                             \"md5sum $fn | cut -f1 -d\' \'> $fn.md5\"\n";
+print $QSUB "qsub -cwd -b y -N ega.$fn.gpg  	                          \"gpg --trust-model always $keystring -o $fn.gpg -e $fn\"\n";
+print $QSUB "qsub -cwd -b y -N ega.$fn.postmd5 -hold_jid ega.$fn.gpg      \"md5sum $fn.gpg | cut -f1 -d\' \'> $fn.gpg.md5\"\n";
+print $QSUB "qsub -cwd -b y -N ega.$fn.upload  -hold_jid ega.$fn.postmd5  \"bash upload.sh\"\n";
+print $QSUB "qsub -cwd -b y -N ega.$fn.rmgpg   -hold_jid ega.$fn.upload   \"rm $fn.gpg;\"\n"  if($opts{delete} eq "True");
 
-#(open my $UP,">","$dir/upload.sh") || die "unable to open upload script";
-#print $UP "ssh xfer.hpc.oicr.on.ca \"lftp -u ega-box-137,GnwyrZm6 -e \\\"set ftp:ssl-allow false; mput $pwd/$dir/$fn.gpg $pwd/$dir/$fn*.md5 -O $remote_dir; bye;\\\" ftp://ftp-private.ebi.ac.uk\"";
-#close $UP;
+if($opts{next} eq "True"){
+	#### need to run the same command, is there way of echoing this in the program
+	print $QSUB "qsub -cwd -b y  -N ega.$fn.startnext -hold_jid ega.$fn.upload  \"sleep 5m;cd $pwd;$commandline\"\n";
+}
+close $QSUB;
 
 
+print STDERR "Preparing upload script under $workdir\n";
+
+my $script_upload="$workdir/upload.sh";
+(open my $UP,">",$script_upload) || die "unable to open $script_upload";
+print $UP "ssh xfer.hpc.oicr.on.ca \"lftp -u $opts{box},$opts{pw} -e \\\"set ftp:ssl-allow false; mput $workdir/$fn.gpg $workdir/$fn*.md5 -O $opts{boxpath}; bye;\\\" ftp://ftp-private.ebi.ac.uk\"";
+close $UP;
 
 
-#`(cd $dir;bash process.sh)`;	
+print STDERR "Start processing\n";
+`(cd $workdir;bash process.sh)`;	
 	
 
 
 sub validate_options{
-	my ($opts)=@_;
+	my (%opts)=@_;
 	usage("Help requested.") if($opts{help});
 	
-	if(! $$opts{configfile} || ! -e $$opts{configfile}){
-		usage("Configuration file not provided or not found.");
-	}
-	if(! $$opts{dir_queue} || ! -d $$opts{dir_queue}){
+	if($opts{configfile}){
+		 if( ! -e $opts{configfile} ){
+			 usage("Configuration file not provided or not found.");
+		 }else{
+			 my %config;
+			 my @recs=`cat $opts{configfile} | grep ":"`;
+			 map{  chomp;
+			       my($k,$v)=split /:/;
+				   $config{$k}=$v
+			 }@recs;
+			 map{$opts{$_} = $opts{$_} || $config{$_}} keys %config;
+		 }
+	 }
+	 
+	
+	if(! $opts{qdir} || ! -d $opts{qdir}){
 		usage("Directory with queued files not provided or nor found.");
 	}
-	if(! $$opts{dir_work} || ! -d $$opts{dir_work}){
+	if(! $opts{wdir} || ! -d $opts{wdir}){
 		usage("Work directory not provided or nor found.");
+	}
+	
+	if(! $opts{box}){
+		usage("ega-box not provided");
+	}
+	
+	if(! $opts{pw}){
+		usage("password for $opts{box} not provided");
+	}
+	
+	if(! $opts{boxpath}){
+		usage("path on $opts{box} not provided");
+	}
+	
+	if(! $opts{keys}){
+		usage("gpg encryption key(s) not provided");
 	}
 	
 	if($opts{delete}){
@@ -101,19 +139,19 @@ sub validate_options{
 		$opts{next}="True";
 	}
 
-
+	return %opts;
 
 }
 
 sub usage{
 	print "\nega_lftp.pl [options]\n";
 	print "Options are as follows:\n";
-	print "\t--config String/filename. A configuratino file, storing options as key value pairs\n";
-	print "\t--dir_queue  String/directory name.  A directory with a list of files that need to be uploaded.\n";
-	print "\t--dir_work  String/directory name. A directory where the file will be copied, encrypted and md5summed, prior to upload.\n";
+	print "\t--config String/filename. A configuration file, storing options as key value pairs\n";  ### currently not supported
+	print "\t--qdir  String/directory name.  A directory with a list of files that need to be uploaded.\n";
+	print "\t--wdir  String/directory name. A directory where the file will be copied, encrypted and md5summed, prior to upload.\n";
 	print "\t--box String.  The ega-box for upload\n";
-	print "\t--box_pass String. Password for the ega-box.\n";
-	print "\t--box_path String. Path on the ega-box where files should be uploaded.\n";
+	print "\t--pw String. Password for the ega-box.\n";
+	print "\t--boxpath String. Path on the ega-box where files should be uploaded.\n";
 	print "\t--delete. True/False.  Should encrypted data be deleted after upload?  Default = True\n";
 	print "\t--next. True/False.  Should the next file be processed once this one has completed.  Default = True.\n";
 	print "\t--keys. String. A comma separated list of public gpg encryption keys..\n";
